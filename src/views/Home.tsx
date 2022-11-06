a927875fcf95fceacbdcc951e024003ebd89abdc
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-11-06 19:07:47 -0300

    feat: :sparkles: Finalização da Home
    Finalização da Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,15 @@
+import PageTitle from "../components/PageTitle";
+
 function Home() {
     return (
-        <p>Home</p>
+        <>
+            <PageTitle title="Página inicial" subtitle="Algumas informações relevantes." />
+            <section id="home">
+                <h2>Olá, Letícia!</h2>
+                <h3>Desenvolvi este painel para você conseguir enviar notificações (mensagens) aos usuários do <span className="primary">Let's!</span>, além de ter acesso a alguns dados estatísticos.</h3>
+                <h4>- Ramon</h4>
+            </section>
+        </>
     );
 }
 
