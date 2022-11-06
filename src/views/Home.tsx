081ea5a9f0dc12936cedd9671e373f136ff54504
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-11-06 20:50:08 -0300

    fix: :bug: Ajustes na View Home
    Ajustes na View Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,6 +8,14 @@ function Home() {
                 <h2>Olá, Letícia!</h2>
                 <h3>Desenvolvi este painel para você conseguir enviar notificações (mensagens) aos usuários do <span className="primary">Let's!</span>, além de ter acesso a alguns dados estatísticos.</h3>
                 <h4>- Ramon</h4>
+
+                <div id="home-observacoes">
+                    <h3>Alguma observações: </h3>
+                    <ul>
+                        <li>Por segurança, só é possível enviar uma mensagem por hora.</li>
+                        <li>Revise <b>bem</b> a mensagem antes de enviar, pois não será possível apagá-la.</li>
+                    </ul>
+                </div>
             </section>
         </>
     );
