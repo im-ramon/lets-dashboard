5ce99e634fa63058f3f9a1e2086eb3cf8cee6cbc
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-11-06 23:55:29 -0300

    feat: :sparkles: Finalização do Dashboard
    Finalização do Dashboard

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,135 +1,175 @@
 import PageTitle from "../components/PageTitle";
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
-const data = [
-    {
-        name: 'J',
-        total: 4000,
-    },
-    {
-        name: 'F',
-        total: 3000,
-    },
-    {
-        name: 'M',
-        total: 2000,
-    },
-    {
-        name: 'A',
-        total: 2780,
-    },
-    {
-        name: 'M',
-        total: 1890,
-    },
-    {
-        name: 'J',
-        total: 2390,
-    },
-    {
-        name: 'J',
-        total: 3490,
-    },
-    {
-        name: 'A',
-        total: 3000,
-    },
-    {
-        name: 'S',
-        total: 2000,
-    },
-    {
-        name: 'O',
-        total: 2780,
-    },
-    {
-        name: 'N',
-        total: 1890,
-    },
-    {
-        name: 'D',
-        total: 2780,
-    },
-];
+import { api } from "../services/api";
+import loading from '../assets/images/svg/loading.svg'
+import reload from '../assets/images/svg/reload.svg'
 
 function Dashboard() {
-    const numbers = [
-        'Fotos antigas',
-        'Instagram',
-        'Sites xxx',
-        'Fotos',
-        'Conversar antigas',
-        'Amigos',
-        'Fotos antigas',
-        'Instagram',
-        'Sites xxx',
-        'Fotos',
-        'Conversar antigas',
-        'Amigos',
-        'Sites xxx',
-        'Fotos',
-        'Conversar antigas',
-        'Amigos',
-        'Fotos antigas',
-        'Instagram',
-        'Sites xxx',
-        'Fotos',
-        'Conversar antigas',
-        'Amigos',
-    ];
-    const listItems = numbers.map((numbers) =>
-        <li>{numbers}</li>
+    const [totalUsers, setTotalUsers] = useState(0)
+    const [totalRelapses, setTotalRelapses] = useState(0)
+    const [totalReasons, setTotalReasons] = useState(0)
+    const [listaReasons, setListaReasons] = useState([{ reason: "" }])
+    const [isLoading, setIsLoading] = useState(false)
+    const [chartData, setChartData] = useState([{}])
+
+    async function getStatistics() {
+        setIsLoading(true)
+        const data = localStorage.getItem('@lets:statistics')
+
+        if (!data) {
+            await api.post('/statistics', { user: "leticia", password: "123" })
+                .then(response => {
+                    localStorage.setItem('@lets:statistics', JSON.stringify(response.data))
+                    console.log('Dados externos carregados')
+                    updateStates(response.data)
+                })
+        } else {
+            updateStates(JSON.parse(data))
+            console.log('Dados locais carregados')
+        }
+
+        convertDates(listaReasons)
+        setIsLoading(false)
+    }
+
+    function updateStates(obj: any) {
+        setTotalUsers(obj.totalUsers)
+        setTotalRelapses(obj.totalRelapses)
+        setTotalReasons(obj.totalReasons)
+        setListaReasons(obj.listReasons)
+        setChartData(convertDates(obj.listReasons))
+    }
+
+    function handleRestartData() {
+        localStorage.removeItem('@lets:statistics');
+        getStatistics()
+    }
+
+    function convertDates(list: any) {
+        const listMonths = list.map((el: { reason: string, created_at: Date }) => {
+            return new Date(el.created_at).getMonth() + 1
+        })
+
+        let data = [
+            {
+                name: 'J',
+                total: listMonths.filter((el: number) => el == 1).length,
+            },
+            {
+                name: 'F',
+                total: listMonths.filter((el: number) => el == 2).length,
+            },
+            {
+                name: 'M',
+                total: listMonths.filter((el: number) => el == 3).length,
+            },
+            {
+                name: 'A',
+                total: listMonths.filter((el: number) => el == 4).length,
+            },
+            {
+                name: 'M',
+                total: listMonths.filter((el: number) => el == 5).length,
+            },
+            {
+                name: 'J',
+                total: listMonths.filter((el: number) => el == 6).length,
+            },
+            {
+                name: 'J',
+                total: listMonths.filter((el: number) => el == 7).length,
+            },
+            {
+                name: 'A',
+                total: listMonths.filter((el: number) => el == 8).length,
+            },
+            {
+                name: 'S',
+                total: listMonths.filter((el: number) => el == 9).length,
+            },
+            {
+                name: 'O',
+                total: listMonths.filter((el: number) => el == 10).length,
+            },
+            {
+                name: 'N',
+                total: listMonths.filter((el: number) => el == 11).length,
+            },
+            {
+                name: 'D',
+                total: listMonths.filter((el: number) => el == 12).length,
+            },
+        ]
+
+        return data
+    }
+
+    const listItems = listaReasons.map((item, index) =>
+        <li key={index}>{item.reason}</li>
     );
+
+    useEffect(() => {
+        getStatistics()
+    }, [])
 
 
     return (
         <>
-            <PageTitle title="Dashboard" subtitle="Algumas informações sobre seus usuários." />
+            <div id="header-dashboard">
+                <PageTitle title="Dashboard" subtitle="Algumas informações sobre seus usuários." />
+                <img onClick={() => { handleRestartData() }} src={reload} alt="reload" title="Recarregar dados" />
+            </div>
             <section id="dashboard">
-                <div className="line1">
-                    <div className="dashboard-item">
-                        <h3>Usuários do <span className="primary">Let's!</span></h3>
-                        <h2 className="dashboard-item-qtd">2.520</h2>
-                    </div>
-                    <div className="dashboard-item">
-                        <h3>Quantidade de recaídas</h3>
-                        <h2 className="dashboard-item-qtd">189</h2>
-                    </div>
-                    <div className="dashboard-item">
-                        <h3>Total de motivos para recair</h3>
-                        <h2 className="dashboard-item-qtd">150</h2>
-                    </div>
-                </div>
-                <div className="line2">
-                    <div className="dashboard-item">
-                        <h3>Quantidade de recaídas por mês</h3>
-                        <ResponsiveContainer width="100%" height="100%">
-                            <AreaChart
-                                width={500}
-                                height={400}
-                                data={data}
-                                margin={{
-                                    top: 10,
-                                    right: 30,
-                                    left: 0,
-                                    bottom: 0,
-                                }}
-                            >
-                                <CartesianGrid strokeDasharray="1 1" />
-                                <XAxis dataKey="name" />
-                                <YAxis />
-                                <Tooltip />
-                                <Area type="step" dataKey="total" stroke="#c9382b" fill="#c15147" />
-                            </AreaChart>
-                        </ResponsiveContainer>
-                    </div>
-                    <div className="dashboard-item list">
-                        <h3>Motivos que leveram a recair</h3>
-                        <ul>{listItems}</ul>
-                    </div>
-                </div>
-            </section>
+                {isLoading ? <img src={loading} alt="loading" />
+                    : (
+                        <>
+                            <div className="line1">
+                                <div className="dashboard-item">
+                                    <h3>Usuários do <span className="primary">Let's!</span></h3>
+                                    <h2 className="dashboard-item-qtd">{totalUsers}</h2>
+                                </div >
+                                <div className="dashboard-item">
+                                    <h3>Quantidade de recaídas</h3>
+                                    <h2 className="dashboard-item-qtd">{totalRelapses}</h2>
+                                </div>
+                                <div className="dashboard-item">
+                                    <h3>Total de motivos para recair</h3>
+                                    <h2 className="dashboard-item-qtd">{totalReasons}</h2>
+                                </div>
+                            </div >
+                            <div className="line2">
+                                <div className="dashboard-item">
+                                    <h3>Quantidade de recaídas por mês</h3>
+                                    <ResponsiveContainer width="100%" height="100%">
+                                        <AreaChart
+                                            width={500}
+                                            height={400}
+                                            data={chartData}
+                                            margin={{
+                                                top: 10,
+                                                right: 30,
+                                                left: 0,
+                                                bottom: 0,
+                                            }}
+                                        >
+                                            <CartesianGrid strokeDasharray="1 1" />
+                                            <XAxis dataKey="name" />
+                                            <YAxis />
+                                            <Tooltip />
+                                            <Area type="step" dataKey="total" stroke="#c9382b" fill="#c15147" />
+                                        </AreaChart>
+                                    </ResponsiveContainer>
+                                </div>
+                                <div className="dashboard-item list">
+                                    <h3>Motivos que leveram a recair</h3>
+                                    <ul>{listItems}</ul>
+                                </div>
+                            </div>
+                        </>
+                    )}
+            </section >
+
         </>
     );
 }
