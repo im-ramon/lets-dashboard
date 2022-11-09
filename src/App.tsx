7800b1d2151b2d5b0f31c58bd7e3db68d8854033
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-11-08 22:16:26 -0300

    feat: :sparkles: Adicionando context
    Adicionando context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import reactLogo from './assets/react.svg'
 import './styles/index.scss'
 import { RouterProvider, Route, BrowserRouter } from "react-router-dom";
 import Aside from './components/Aside';
 import Router from './routes/routes';
+import { AppContext } from './contexts/appContext';
+import { AppProvider } from './contexts/appContext';
 
 function App() {
+    const { } = useContext(AppContext)
+
     return (
-        <BrowserRouter>
-            <Aside />
-            <main>
-                <Router />
-            </main>
-        </BrowserRouter>
+        <AppProvider>
+            <BrowserRouter>
+                <Aside />
+                <main>
+                    <Router />
+                </main>
+            </BrowserRouter>
+        </AppProvider>
     )
 }
 
---
+++ src/contexts/appContext.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, createContext, ReactNode } from 'react';
+
+interface AppContextData {
+    user: string;
+}
+
+interface AppProviderProps {
+    children: ReactNode;
+}
+
+export const AppContext = createContext({} as AppContextData)
+
+
+export function AppProvider({ children }: AppProviderProps) {
+    const [user, setUser] = useState<string>('')
+    return (
+        <AppContext.Provider value={{
+            user
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
