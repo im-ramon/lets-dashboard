b0608aeb967919007f79f7ccce3e8dfd13649767
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-12-06 22:42:50 -0300

    feat: :sparkles: Versão de produção
    Versão de produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.6:21080'
+    // baseURL: 'http://192.168.1.6:21080'
     // baseURL: 'http://lojababybaby.com.br:21080'
+    baseURL: 'http://appespecialistax.com.br:21034'
 })
 
 export { api }
