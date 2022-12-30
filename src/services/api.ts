dbe9d58b296288b8612b81495eece46cb5ece22f
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-12-30 14:44:25 -0300

    feat: :sparkles: Melhoramento na API
    Melhoramento na API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: 'http://192.168.1.6:21080'
     // baseURL: 'http://lojababybaby.com.br:21080'
-    baseURL: 'http://appespecialistax.com.br:21034'
+    // baseURL: 'http://appespecialistax.com.br:21141',
+    baseURL: 'https://api.appespecialistax.com.br',
 })
 
 export { api }
